75564bd2efcb5dc8f590007e114a74234f45f97e
msimitz <91494191+msimitz@users.noreply.github.com>
2022-01-19 17:03:12 -0800

    Fixed bug with non-relevant brackets and parenthesis

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,13 +11,20 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            System.out.println(currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+
+            int markdownCheck = nextCloseBracket + 1;
+            if(markdown.charAt(markdownCheck) == ('(')) {
+                int openParen = markdown.indexOf("(", nextCloseBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            else {
+                currentIndex = markdownCheck;
+            }
+
         }
         return toReturn;
     }
